9476ff49dbc3d8b4a6bd406709a192d92d961775
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-10 12:53:41 +0000

    Co-authored-by: cjp45 <cjp45@users.noreply.github.com> Co-authored-by: notnotshrek <notnotshrek@users.noreply.github.com>

---
+++ group project/litapp.py
@@ -0,0 +1,29 @@
+#______-Basic Layout-_______
+# (headers, footers text)
+import streamlit as st
+
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+multi = '''If you end a line with two spaces,
+a soft return is used for the next line.
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
+
+
+
+
+
+
+
+
+
+#______-"Backend code"-_______
+# (python code supporting the layout code)
+
