e72d5579f8efd3996308481c63f02034cd2e127b
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-10 13:00:17 +0000

    farttt changes

--- group project/litapp.py
+++ group project/litapp.py
@@ -2,22 +2,11 @@
 # (headers, footers text)
 import streamlit as st
 
-st.markdown("*Streamlit* is **really** ***cool***.")
-st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
-
-Two (or more) newline characters in a row will result in a hard return.
-'''
-st.markdown(multi)
-
-
-
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
 
 
 #______-"Backend code"-_______
