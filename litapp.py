fe86e36cf43e575ef004e5c8be0e3df5003d906e
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-10 13:51:54 +0000

    YEAH
    Co-authored-by: notnotshrek <notnotshrek@users.noreply.github.com>

--- litapp.py
+++ litapp.py
@@ -1,5 +1,3 @@
-#______-Basic Layout-_______
-# (headers, footers text)
 import streamlit as st
 
 item1 = "chicken"
@@ -9,28 +7,20 @@ item2points = 0
 item3 = "Rahhh"
 item3points = 0
 
-import streamlit as st
-
 foodselection = st.radio(
     "Today's Lunch",
-    [f':rainbow[{item1}]', f"***{item2}***", f'{item3} :movie_camera:'],
-    captions = ["Joke", "No.", "Test"])
+    [f':rainbow: {item1}', f"***{item2}***", f'{item3} :movie_camera:'],
+    format_func=lambda x: "Joke" if x == f':rainbow: {item1}' else "No." if x == f"***{item2}***" else "Test")
 
-if foodselection == f'{item1}':
-    st.button("Reset", type="primary")
+if foodselection == f':rainbow: {item1}':
     if st.button("Vote"):
         item1points += 1
-    st.button("Vote")
-elif foodselection == f'{item2}':
-    st.button("Reset", type="primary")
+elif foodselection == f"***{item2}***":
     if st.button("Vote"):
         item2points += 1
-    st.button("Vote")
-elif foodselection == f'{item3}':
-    st.button("Reset", type="primary")
+elif foodselection == f'{item3} :movie_camera:':
     if st.button("Vote"):
         item3points += 1
-    st.button("Vote")
 
 col1, col2, col3 = st.columns(3)
 col1.metric("Item One", f"{item1}", f"{item1points}")
@@ -38,6 +28,4 @@ col2.metric("Item Two", f"{item2}", f"{item2points}")
 col3.metric("Item Three", f"{item3}", f"{item3points}")
 
 st.caption('HELLO.')
-st.caption('Swag :blue[Swag] and Swag :sunglasses:')
-#______-"Backend code"-_______ 
-# (python code supporting the layout code)
+st.caption('Swag :blue_square: Swag :sunglasses:')
