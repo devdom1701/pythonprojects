44242d20e1f42a8cbce7a721fa78868d28de697e
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-10 13:44:16 +0000

    Paaa

--- litapp.py
+++ litapp.py
@@ -1,5 +1,7 @@
 #______-Basic Layout-_______
 # (headers, footers text)
+import streamlit as st
+
 item1 = "chicken"
 item1points = 0
 item2 = "phart"
@@ -15,17 +17,27 @@ foodselection = st.radio(
     captions = ["Joke", "No.", "Test"])
 
 if foodselection == f'{item1}':
-    item1points += 1
+    st.button("Reset", type="primary")
+    if st.button("Vote"):
+        item1points += 1
+    st.button("Vote")
 elif foodselection == f'{item2}':
-    item2points += 1
+    st.button("Reset", type="primary")
+    if st.button("Vote"):
+        item2points += 1
+    st.button("Vote")
 elif foodselection == f'{item3}':
-    item3points += 1
+    st.button("Reset", type="primary")
+    if st.button("Vote"):
+        item3points += 1
+    st.button("Vote")
 
 col1, col2, col3 = st.columns(3)
 col1.metric("Item One", f"{item1}", f"{item1points}")
 col2.metric("Item Two", f"{item2}", f"{item2points}")
 col3.metric("Item Three", f"{item3}", f"{item3points}")
 
-#______-"Backend code"-_______
+st.caption('HELLO.')
+st.caption('Swag :blue[Swag] and Swag :sunglasses:')
+#______-"Backend code"-_______ 
 # (python code supporting the layout code)
-
