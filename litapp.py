073d6ee3c1fe5f08025d5718e68455dea32cf808
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-13 21:26:18 +0000

    Huh

--- litapp.py
+++ litapp.py
@@ -56,16 +56,19 @@ if signed_in:
     else:
         st.warning('Already Voted Bozo')
 
-    if Voted:
-        tab1 = st.tabs(["Comment"])
-        with tab1:
-            # Comments
-            st.header('Comments')
-            with st.container():
-                prompt = st.text_input("Say something")
-                if prompt:
-                    messages = st.container()
-                    messages.markdown(f'{usernamelist[usernamelistcounter - 1]}: {prompt}', unsafe_allow_html=True)  # Adjusted counter
+if Voted:
+    tab1 = st.tabs(["Comment"])
+    with tab1:
+        # Comments
+        st.header('Comments')
+        with st.container():
+            prompt = st.text_input("Say something")
+            if prompt:
+                messages = st.container()
+                messages.markdown(f'{usernamelist[usernamelistcounter - 1]}: {prompt}', unsafe_allow_html=True)
 
 else:
-    st.warning("Sign in to continue..")
+    if signed_in:
+        st.warning("Vote to unlock the comment section..")
+    else:
+        st.warning("Sign in to continue..")
