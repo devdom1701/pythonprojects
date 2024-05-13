06a81bdece052bb051b9ea1652b98220bbdf6597
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-13 21:19:29 +0000

    ya

--- litapp.py
+++ litapp.py
@@ -55,15 +55,16 @@ if signed_in:
                 Voted = True
         else:
             st.warning('Already Voted Bozo')
-if Voted:
-    tab1 = st.tabs(["Comment"])
-    with tab1:
-        # Comments
-        st.header('Comments')
-        with st.container():
-            prompt = st.text_input("Say something")
-            if prompt:
-                messages = st.container()
-                messages.markdown(f'{usernamelist[usernamelistcounter]}: {prompt}', unsafe_allow_html=True)
 else:
-    st.warning("Sign in to continue..")
+    if Voted:
+        tab1 = st.tabs(["Comment"])
+        with tab1:
+            # Comments
+            st.header('Comments')
+            with st.container():
+                prompt = st.text_input("Say something")
+                if prompt:
+                    messages = st.container()
+                    messages.markdown(f'{usernamelist[usernamelistcounter]}: {prompt}', unsafe_allow_html=True)
+        else:
+            st.warning("Sign in to continue..")
