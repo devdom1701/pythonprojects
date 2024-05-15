a1054e4b2c1bbbbcf5c65f2471ac698526e76b32
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-15 22:47:37 +0000

    added voting class om

--- litapp.py
+++ litapp.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from datetime import datetime
 
+class VotingOption:
+    def __init__(self, name):
+        self.name = name
+        self.votes = 0
+
+    def vote(self):
+        self.votes += 1
+
 if 'signincounter' not in st.session_state:
     st.session_state.signincounter = 0
 if 'votedcounter' not in st.session_state:
@@ -9,8 +17,8 @@ if 'Voted' not in st.session_state:
     st.session_state.Voted = False
 if 'signed_in' not in st.session_state:
     st.session_state.signed_in = False
-if 'votes' not in st.session_state:
-    st.session_state.votes = {food: 0 for food in ["fart", "chicken", "poopp", "bunger", "Fungus", "Mold", "Starvation"]}
+if 'voting_options' not in st.session_state:
+    st.session_state.voting_options = [VotingOption(name) for name in ["fart", "chicken", "poopp", "bunger", "Fungus", "Mold", "Starvation"]]
 
 st.title("Voting for Food App")
 
@@ -33,12 +41,12 @@ if st.sidebar.button("Sign In"):
 # Voting
 if st.session_state.signed_in and not st.session_state.Voted:
     st.header('Voting')
-    for food in ["fart", "chicken", "poopp", "bunger", "Fungus", "Mold", "Starvation"]:
-        button = st.button(f'Vote for "{food}"')
+    for option in st.session_state.voting_options:
+        button = st.button(f'Vote for "{option.name}"')
         if button:
-            st.toast(f'Press Again to vote for {food}')
+            st.toast(f'Press Again to vote for {option.name}')
             st.session_state.votedcounter += 1
-            st.session_state.votes[food] += 1
+            option.vote()
             st.session_state.Voted = True
 
 # Comments
@@ -46,11 +54,13 @@ if st.session_state.Voted and st.session_state.signed_in:
     st.header('Comments')
     prompt = st.text_input("Say something")
     if prompt:
-        timestamp = datetime.now().strftime("%Y/%m/%d %H:%M")
-        st.markdown(f'{username}, At [{timestamp}]: {prompt}', unsafe_allow_html=True)
+        timestamp = datetime.now().strftime("%H:%M")
+        st.markdown(f'{username} , at {timestamp}: {prompt}', unsafe_allow_html=True)
 elif not st.session_state.signed_in:
     st.sidebar.warning("Sign in to continue.")
 
+# Show voting results
 if st.session_state.Voted and st.session_state.signed_in:
     st.header('Voting Results')
-    st.bar_chart(st.session_state.votes)
+    votes_dict = {option.name: option.votes for option in st.session_state.voting_options}
+    st.bar_chart(votes_dict)
