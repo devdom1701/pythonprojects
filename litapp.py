64b1cd4611798bdaaeb5bc6761d168dbe41d3ddc
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-16 02:11:36 +0000

    haiiii

--- litapp.py
+++ litapp.py
@@ -36,36 +36,37 @@ def sign_in(username, password):
     else:
         st.sidebar.warning("Please enter a valid username and password.")
 
+# Sign-in inputs
 username = st.sidebar.text_input("Username")
 password = st.sidebar.text_input("Password", type="password")
 
+# Sign-in button
 if st.sidebar.button("Sign In"):
     sign_in(username, password)
 
-# Voting
-if st.session_state.signed_in and not st.session_state.Voted:
-    st.header('Voting')
-    for option in st.session_state.voting_options:
-        button = st.button(f'Vote for "{option.name}"')
-        if button:
-            st.toast(f'Press Again to vote for {option.name}')
-            st.session_state.votedcounter += 1
-            option.vote()
-            st.session_state.Voted = True
+# Voting section
+if st.session_state.signed_in:
+    if not st.session_state.Voted:
+        st.header('Voting')
+        for option in st.session_state.voting_options:
+            if st.button(f'Vote for "{option.name}"'):
+                st.toast(f'Press Again to vote for {option.name}')
+                st.session_state.votedcounter += 1
+                option.vote()
+                st.session_state.Voted = True
+    else:
+        st.header('Comments')
+        prompt = st.text_input("Say something")
+        if prompt:
+            timestamp = datetime.now().strftime("%H:%M")
+            st.session_state.messages[username].insert(0, f'{username} , at {timestamp}: {prompt}')
 
-# Comments
-if st.session_state.Voted and st.session_state.signed_in:
-    st.header('Comments')
-    prompt = st.text_input("Say something")
-    if prompt:
-        timestamp = datetime.now().strftime("%H:%M")
-        st.session_state.messages[username].append(f'{username} , at {timestamp}: {prompt}')
-
-    for msg in st.session_state.messages[username]:
-        st.markdown(msg, unsafe_allow_html=True)
-elif not st.session_state.signed_in:
+        for msg in reversed(st.session_state.messages[username]):
+            st.markdown(msg, unsafe_allow_html=True)
+else:
     st.sidebar.warning("Sign in to continue.")
 
+# Voting Results
 if st.session_state.Voted and st.session_state.signed_in:
     st.header('Voting Results')
     votes_dict = {option.name: option.votes for option in st.session_state.voting_options}
