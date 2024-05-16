36cfcd85cc94991a5213eaa1b42a8706cd891bfd
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-16 13:50:22 +0000

    phart

--- litapp.py
+++ litapp.py
@@ -1,30 +1,6 @@
 import streamlit as st
 import random
 from datetime import datetime
-import pyodbc
-
-# Database configuration
-server = 'PhartSQL'
-database = 'sa'
-username = 'sa'
-password = 'dockerStrongPwd123'
-connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
-
-# Establish the database connection
-conn = pyodbc.connect(connection_string)
-cursor = conn.cursor()
-
-# Create the comments table if it doesn't exist
-cursor.execute('''
-    IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='comments' AND xtype='U')
-    CREATE TABLE comments (
-        id INT IDENTITY(1,1) PRIMARY KEY,
-        username NVARCHAR(50) NOT NULL,
-        timestamp NVARCHAR(50) NOT NULL,
-        message NVARCHAR(MAX) NOT NULL
-    )
-''')
-conn.commit()
 
 class VotingOption:
     def __init__(self, name):
@@ -35,13 +11,6 @@ class VotingOption:
         self.votes += 1
 
 food_list2 = ["Pizza", "Chicken Tenders", "Chicken Alfredo", "Bacon Cheeseburger", "Bosco Sticks", "Chicken Quesadilla", "Chicken Sandwich", "French Toast", "Pizza Crunchers", "General Tso's", "Walking Tacos", "Buffalo Chicken Pizza", "Chicken Fajita Bowl", "Popcorn Chicken Bowl", "Taco Tuesday", "Cheeseburger", "Pork Carnitas", "Pasta w/ Meat Sauce", "Loaded Potato Wedges", "Chicken Nuggets", "Grilled Cheese", "Brisket and Potato Bowl", "Chicken Strips", "Cheeseburger Waffle Fries", "Cheese Rippers", "BBQ Pork Sandwich", "Cheese Ravioli w/ Meat Sauce", "Chicken Carnita Bowl", "Philly Cheesesteak", "Ravioli", "Sweet and Sour Chicken", "Pepperoni Stuffed Breadstick", "Meatballs Subs","slop","Crabby Paddi"]
-
-# Randomly select choices from the food list
-choice = food_list2[random.randint(0, len(food_list2) - 1)]
-choice2 = food_list2[random.randint(0, len(food_list2) - 1)]
-choice3 = food_list2[random.randint(0, len(food_list2) - 1)]
-
-randomchoicelist = [choice, choice2, choice3]
 
 if 'signincounter' not in st.session_state:
     st.session_state.signincounter = 0
@@ -54,7 +23,9 @@ if 'notcommented' not in st.session_state:
 if 'signed_in' not in st.session_state:
     st.session_state.signed_in = False
 if 'voting_options' not in st.session_state:
-    st.session_state.voting_options = [VotingOption(name) for name in randomchoicelist]
+    st.session_state.voting_options = [VotingOption(name) for name in ["fart", "chicken", "poopp", "bunger", "Fungus", "Mold", "Starvation","Rat Burg","Air(Personal Favorite)","Ice and Milk"]]
+if 'messages' not in st.session_state:
+    st.session_state.messages = {}
 
 if not st.session_state.Voted:
     st.title("Voting for Food App")
@@ -66,6 +37,8 @@ def sign_in(username, password):
         st.toast("Signed In")
         st.session_state.signed_in = True
         st.session_state.signincounter += 1
+        if username not in st.session_state.messages:
+            st.session_state.messages[username] = []
     else:
         st.sidebar.warning("Please enter a valid username and password.")
 
@@ -94,17 +67,11 @@ if st.session_state.Voted and st.session_state.signed_in:
     prompt = st.text_input("Say something")
     if prompt:
         timestamp = datetime.now().strftime("%H:%M")
-        cursor.execute('''
-            INSERT INTO comments (username, timestamp, message)
-            VALUES (?, ?, ?)
-        ''', (username, timestamp, prompt))
-        conn.commit()
-        st.session_state.notcommented = False
+        st.session_state.messages[username].append(f'{username} , at {timestamp}: {prompt}')
 
-    cursor.execute('SELECT username, timestamp, message FROM comments WHERE username = ?', (username,))
-    comments = cursor.fetchall()
-    for comment in comments:
-        st.markdown(f'{comment[0]}, at {comment[1]}: {comment[2]}', unsafe_allow_html=True)
+    for msg in st.session_state.messages[username]:
+        st.markdown(msg, unsafe_allow_html=True)
+        st.session_state.notcommented = False
 elif not st.session_state.signed_in:
     st.sidebar.warning("Sign in to continue.")
 
