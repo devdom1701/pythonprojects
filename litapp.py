6d5af04437c2ea2a3fa9b7d819d03409b546879e
devdom <84544217+devdom1701@users.noreply.github.com>
2024-05-22 13:07:17 +0000

    yeaaaa

--- litapp.py
+++ litapp.py
@@ -14,38 +14,38 @@ class VotingOption:
 
 # Dictionary for images and descriptions
 food_details = {
-    "Pizza": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Cheese or peppaoni pizza"},
-    "Chicken Tenders": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Chicken tenders"},
-    "Chicken Alfredo": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Pasta with alfredo sauce and chicken"},
-    "Bacon Cheeseburger": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "A cheeseburger with bacon on it"},
-    "Bosco Sticks": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Sticks with cheese"},
-    "Chicken Quesadilla": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Quesadilla but chicken"},
-    "Chicken Sandwich": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Checken inbtweem sum buns"},
-    "French Toast": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "French but toast flavored"},
-    "Pizza Crunchers": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Pizza but crunchy"},
-    "General Tso's": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Chicken or something"},
-    "Walking Tacos": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Taco but it walks"},
-    "Buffalo Chicken Pizza": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Pizza but with buffalos on it"},
-    "Chicken Fajita Bowl": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Chicken in a bowl"},
-    "Popcorn Chicken Bowl": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Popcorn version of chicken in a bowl"},
-    "Cheeseburger": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "ChesBurger"},
-    "Pork Carnitas": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Pork Carnitas, thats just the description you're good."},
-    "Pasta w/ Meat Sauce": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Pasta with cool sauce on it."},
-    "Loaded Potato Wedges": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Loaded Potato wedges, Thats it."},
-    "Chicken Nuggets": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Chicken with nuggies."},
-    "Grilled Cheese": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Cheese but on the grill"},
-    "Brisket and Potato Bowl": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Chicken Strips": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Cheeseburger Waffle Fries": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Cheese Rippers": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "BBQ Pork Sandwich": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Cheese Ravioli w/ Meat Sauce": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Philly Cheesesteak": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Ravioli": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Sweet and Sour Chicken": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Pepperoni Stuffed Breadsticks": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Meatball Subs": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
-    "Slop": {"image": "https://i.imgur.com/4AiIiq3.gif", "description": "Caleb"},
+    "Pizza": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Cheese or peppaoni pizza"},
+    "Chicken Tenders": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Chicken tenders"},
+    "Chicken Alfredo": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Pasta with alfredo sauce and chicken"},
+    "Bacon Cheeseburger": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "A cheeseburger with bacon on it"},
+    "Bosco Sticks": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "A breadstick with cheese inside"},
+    "Chicken Quesadilla": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "A quesadilla with chicken"},
+    "Chicken Sandwich": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Checken inbtweem sum buns"},
+    "French Toast": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "French toast, a golden-brown, crispy yet fluffy slice of heaven that begins with thick slices of bread soaked in a rich, creamy mixture of beaten eggs, milk, vanilla extract, and a dash of cinnamon, which, when cooked to perfection on a hot griddle, transforms into a mouthwatering breakfast delicacy that’s both sweet and savory, with its crispy edges and soft, custard-like center, making it a favorite for breakfast or brunch, especially when generously topped with a pat of melting butter, a drizzle of warm maple syrup, fresh berries, or even a dusting of powdered sugar, creating a symphony of flavors and textures that dance on your taste buds and make every bite a delightful experience, reminiscent of cozy Sunday mornings, family gatherings, and the simple joys of comfort food, where each bite transports you to a place of warmth and satisfaction, blending the sweetness of the syrup with the subtle spice of the cinnamon, and the richness of the custard with the crispiness of the toasted bread, truly making French toast not just a meal, but a nostalgic, comforting experience that brings joy, contentment, and a little bit of magic to your morning routine, because let’s be real, who can resist the allure of a perfectly made French toast, with its irresistible aroma and heavenly taste that just makes you want to savor every single bite, and maybe even go back for seconds, because it’s just that good, and no cap, French toast is straight-up mogging every other breakfast out there, turning your average morning into something special, something unforgettable, something that makes you want to wake up early just to enjoy this delectable treat, knowing that every bite is worth it, and that French toast, with its blend of simplicity and indulgence, has earned its rightful place as a beloved breakfast staple, adored by many, and forever a classic in the culinary world. Also top it with sum slop instead of syrup"},
+    "Pizza Crunchers": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Mozzarella and pizza sauce covered in breadcrumbs"},
+    "General Tso's": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Orange chicken, with rice"},
+    "Walking Tacos": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Ground beef and taco toppings in a bag of Doritos"},
+    "Buffalo Chicken Pizza": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Chicken served on seasoned rice"},
+    "Chicken Fajita Bowl": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Popcorn chicken on mashed potatoes covered in gravy"},
+    "Popcorn Chicken Bowl": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Popcorn chicken on mashed potatoes covered in gravy"},
+    "Cheeseburger": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "CCheeseburger3"},
+    "Pork Carnitas": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Pork Carnitas, thats just the Description you're good."},
+    "Pasta w/ Meat Sauce": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Pasta with cool sauce on it."},
+    "Loaded Potato Wedges": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Loaded Potato wedges, Thats it."},
+    "Chicken Nuggets": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Chicken with nuggies."},
+    "Grilled Cheese": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Cheese but on the grill"},
+    "Brisket and Potato Bowl": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "           "},
+    "Chicken Strips": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Chicken but it stips."},
+    "Cheeseburger Waffle Fries": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Burger but with waffles by it"},
+    "Cheese Rippers": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Cheese but grim ripper"},
+    "BBQ Pork Sandwich": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "BB Pork with no Q also its a sandwitch"},
+    "Cheese Ravioli w/ Meat Sauce": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Tasty meat."},
+    "Philly Cheesesteak": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Cheesesteak but Philly(I dont care if it originated in Philly)"},
+    "Ravioli": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "An italian delecasy suited for only the finest in sociotey, those who taste it initally for the first time in their lives will explode and be in shock and awe at the sheer desnsity of the pasta inside this elegant, but simple dish; although to the untrained eye it may seem a contrevertial or irrelevant food, However the ravioli is just the dish for your sad saturday morning."},
+    "Sweet and Sour Chicken": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Sour chicken with the beef"},
+    "Pepperoni Stuffed Breadsticks": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Breadsticks with cheese and alittle bit a slop with peperoini also im seeing how long i can make the commit before it bugs out so just keep reading I swear this is a Real representation of how this food taste So just eat it because i think its very tastely"},
+    "Meatball Subs": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "Meatbull but subbed out."},
+    "Slop": {"image": "https://i.imgur.com/4AiIiq3.gif", "Description": "It taste good just try it. Bi't minty but you'll get used to it. Also we got it from Shrek's swamp"},
 }
 
 food_list2 = ["Pizza", "Chicken Tenders", "Chicken Alfredo", "Bacon Cheeseburger", "Bosco Sticks", "Chicken Quesadilla", 
@@ -54,7 +54,7 @@ food_list2 = ["Pizza", "Chicken Tenders", "Chicken Alfredo", "Bacon Cheeseburger
               "Loaded Potato Wedges", "Chicken Nuggets", "Grilled Cheese", "Brisket and Potato Bowl", "Chicken Strips", 
               "Cheeseburger Waffle Fries", "Cheese Rippers", "BBQ Pork Sandwich", "Cheese Ravioli w/ Meat Sauce", 
              "Philly Cheesesteak", "Ravioli", "Sweet and Sour Chicken", "Pepperoni Stuffed Breadstick", 
-              "Meatballs Subs"]
+              "Meatballs Subs", "Slop"]
 
 if 'signincounter' not in st.session_state:
     st.session_state.signincounter = 0
@@ -68,7 +68,7 @@ if 'signed_in' not in st.session_state:
     st.session_state.signed_in = False
 if 'voting_options' not in st.session_state:
     st.session_state.voting_options = [
-        VotingOption(name, food_details[name]['image'], food_details[name]['description']) 
+        VotingOption(name, food_details[name]['image'], food_details[name]['Description']) 
         for name in food_list2 if name in food_details
     ]
 if 'messages' not in st.session_state:
